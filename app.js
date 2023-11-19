7f6cbb8dfc62419d8864d99a5cfeea86b6935d62
Melih Erpek <61746050+MelihErpek@users.noreply.github.com>
2023-11-19 18:42:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const openai = new OpenAI({
-  apiKey: process.env.DB_HOST,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const upload = multer({ dest: "uploads/" });
 
