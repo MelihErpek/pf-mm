b316879743140ac3a6ff1f3504de6bdcc203f62d
Melih Erpek <61746050+MelihErpek@users.noreply.github.com>
2023-11-27 17:35:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const openai = new OpenAI({
 app.get("/", async (req, res) => {
 const filePath = path.join(__dirname, 'data.jsonl');
   console.log(filePath)
-  res.json(filePath);
+  res.json(filePath+"selam");
 });
 
 
